aed1f3eee2ed474e3a0f1e08ddc25920355d10f8
Thinslices Academy #17 <thinslicesacademy17@Andrews-MacBook.thinslices.com>
2025-07-30 15:22:52 +0300

    Added frontend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
